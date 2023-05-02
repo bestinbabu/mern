fac7e97ac91b368724ac83a1587b20f62c55e55d
bestinbabu <bestinbabu6@gmail.com>
2023-05-02 15:29:58 +0530

    day1

---
+++ script.js
@@ -0,0 +1,122 @@
+// // // // const name = "bestin";
+// // // // console.log(name);
+
+// // // // console.log(
+// // // //   5 % 2,
+// // // //   5 + 5,
+// // // //   5 == 5,
+// // // //   5 - 5,
+// // // //   5 * 5,
+// // // //   5 ** 5,
+// // // //   5 != 7,
+// // // //   5 != 5,
+// // // //   5 > 6,
+// // // //   5 < 7,
+// // // //   "5" === 5
+// // // // );
+// // // // console.log("bestin" + " babu");
+
+// // // // const lname = "nba";
+// // // // console.log(`${name} ${lname}`);
+
+// // // // let car = {
+// // // //   name: "bestin",
+// // // //   color: "black",
+// // // // };
+// // // // console.log(car);
+
+// // // // let person = {
+// // // //   name: "bestin",
+// // // //   clg: "christ ",
+// // // //   roll: 5,
+// // // //   address: {
+// // // //     houseName: "kannaikkal house",
+// // // //   },
+// // // // };
+// // // // console.log(person);
+
+// // // // let array = [1, 2, 3, 4];
+// // // // console.log(array[2]);
+
+// // // // array.push(123);
+// // // // console.log(array);
+
+// // // // array.pop();
+
+// // // // const arrayOfObjects = [
+// // // //   {
+// // // //     title: "eat",
+// // // //   },
+// // // //   {
+// // // //     title: "sleep",
+// // // //   },
+// // // //   {
+// // // //     title:"wake"
+// // // //   }
+// // // // ];
+
+
+
+// // // // const add = function (a,b) {
+// // // //     const sum = a + b
+// // // //     console.log("ok hi",sum)
+// // // // }
+
+// // // // add(1,2)
+
+// // // // const subtract = function (a,b) {
+// // // //     const sum = a - b
+// // // //     console.log("ok hi",sum)
+// // // // }hi
+
+// // // // subtract(1,2)
+
+
+// // // // const ok = () => {
+// // // //     console.log("ok bye");
+// // // // }
+// // // // ok()
+
+
+// // // const fun = (a,b) => {
+// // //     if((a+b) > 10) {
+// // //         console.log("greater than 10");
+        
+// // //     }
+// // //     else {
+// // //         console.log("less than 10");
+// // //     }
+// // // }
+
+// // // fun(1,99)
+
+// const num = [1,2,3,4,5,5,6,7,8,9]
+
+// // const print = (array) => {
+// //     for(i =0; i< array.length;i++){
+// //         console.log(array[i]);
+// //     }
+// // }
+// // print(num)
+
+// // const whiel = (array) => {
+// //     let length = array.length
+// //     while(length >= 0) {
+// //         console.log(array[length]);
+// //         length--
+// //     }
+// // }
+// // whiel(num)
+
+// const dowhile = () => {
+//  let length = num.length
+
+//     do{
+//         console.log(num[length]);
+//         length--
+
+//     }while(length>=0)
+// }
+
+// dowhile()
+
